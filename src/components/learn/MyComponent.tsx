56025bcff0e5a7ac736503c58db1823e8a18b699
duongdong <dong.dq225808@sis.hust.edu.vn>
2025-07-18 22:33:05 +0700

    Variables and JSON.stringify

--- src/components/learn/MyComponent.tsx
+++ src/components/learn/MyComponent.tsx
@@ -3,11 +3,19 @@
 //fragment
 import './style.css'
 const MyComponent = () => {
+    const duongdong = "Duong Dong"; //string
+    //const duongdong = 26; //number
+    //const duongdong = true; //boolean
+    //const duongdong = undefined; //undefined
+    //const duongdong = null; //null
 
+    //const duongdong = [1, 2, 3];
+    //const duongdong = { name: 'Duong Dong', age: 26 }; //object
     return (
         //React Fragment
         <>
-            <div>Duong Dong new</div>
+            <div> {JSON.stringify(duongdong)} new</div>
+            {/* <div>{console.log("Dong")}</div> */}
             <div className="child"
                 style={{ borderRadius: '10px' }}>child</div>
 
