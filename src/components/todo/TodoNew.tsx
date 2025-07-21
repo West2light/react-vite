293bed9be9e133390a93b6168be8eb17c0f087c8
DuongDong <dong.dq225808@sis.hust.edu.vn>
2025-07-21 10:29:12 +0700

    onClick, onChange with React

--- src/components/todo/TodoNew.tsx
+++ src/components/todo/TodoNew.tsx
@@ -5,10 +5,16 @@ const TodoNew = (props: TodoNewProps) => {
     console.log(">>> check props: ", props);
     const { addNewTodo } = props;
     //addNewTodo('Dong Arsenal');
+    const handleClick = () => {
+        alert('Click me');
+    }
+    const handleOnChange = (name: string) => {
+        console.log('Change input: ', name);
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" onChange={(event) => handleOnChange(event.target.value)} />
+            <button style={{ cursor: "pointer" }} onClick={handleClick}>Add</button>
         </div>
     );
 }
